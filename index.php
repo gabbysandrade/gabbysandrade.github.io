f36a6ade37b5dca5b7eafb1eb24c6dbcb04ec07e
gabbysandrade <gabbysandrade@gmail.com>
2022-10-18 22:02:50 -0500

    load mustache library and create new template engine

--- index.php
+++ index.php
@@ -6,3 +6,10 @@ Purpose: This program will render the homepage for a properly structured mobile-
 mobile-first, responsive portfolio website, Floração Design, using templates created using
 the mustache library.
 **************************/
+
+// This will load the mustache template library
+require_once 'mustache/mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+// This will create a new mustache template engine
+$mustache = new Mustache_Engine;
