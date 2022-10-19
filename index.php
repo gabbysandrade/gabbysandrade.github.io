4e166c08970d59a55e1d2ab3f15efed23b16c6ac
gabbysandrade <gabbysandrade@gmail.com>
2022-10-18 22:02:09 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This program will render the homepage for a properly structured mobile-friendly, 
+mobile-first, responsive portfolio website, Floração Design, using templates created using
+the mustache library.
+**************************/
