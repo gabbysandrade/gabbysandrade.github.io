59f34accb1d8f9dfa433e184a7e3b4655f023cf2
gabbysandrade <gabbysandrade@gmail.com>
2022-10-19 12:37:10 -0500

    initial commit

---
+++ js/activelink.js
@@ -0,0 +1,6 @@
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This script will move the class of "active" to the appropriate
+link in the navbar depending upon the page title.
+**************************/
