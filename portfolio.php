b67babca4b1fbb3ede0e4d2e7eed0aa278fa1c3e
gabbysandrade <gabbysandrade@gmail.com>
2022-10-19 13:59:01 -0500

    initial commit

---
+++ portfolio.php
@@ -0,0 +1,9 @@
+<?php
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This program will render the Portfolio page for a properly structured mobile-friendly, 
+mobile-first, responsive portfolio website, Floração Design, using templates created using
+the mustache library. The portfolio page will feature cards describing and linking to
+portfolio entries.
+**************************/
