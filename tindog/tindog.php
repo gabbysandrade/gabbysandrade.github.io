1a4d3f0cebfe9b8801923e013d3c1d9f0afe6c22
gabbysandrade <gabbysandrade@gmail.com>
2022-11-10 10:15:39 -0600

    load mustache template library and create new mustache template engine

--- tindog/tindog.php
+++ tindog/tindog.php
@@ -5,3 +5,10 @@ Floração Design
 Purpose: This program will render the launch page for a fictional startup app,
 tindog.
 **************************/
+
+// This will load the mustache template library
+require_once '../mustache/mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+// This will create a new mustache template engine
+$mustache = new Mustache_Engine;
