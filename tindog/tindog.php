3148993c1eafcfc7910b500ab1784901791b798b
gabbysandrade <gabbysandrade@gmail.com>
2022-11-10 10:14:38 -0600

    initial commit

---
+++ tindog/tindog.php
@@ -0,0 +1,7 @@
+<?php
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This program will render the launch page for a fictional startup app,
+tindog.
+**************************/
