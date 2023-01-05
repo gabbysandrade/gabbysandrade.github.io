ade6cfab8180a3a5b611616f35fd6fce858c2bd2
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 12:16:19 -0600

    edit footer to match floracao template

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,17 +1,19 @@
 <!--------------------------
 Name: Gabriela Andrade
-Intermediate Web Programming
-Purpose: This program will generate the footer template for the
-comer o sol web application.
+Floração Design
+Purpose: This template will render the document "footer" of each page
+of the Floração website. It will include the document footer and all
+universal scripts for the pages.
 --------------------------->
-        <footer class="text-center py-3 mt-5">
-            <?= $title ?><br>
-            <h6 class="site-title pt-3">comerosol</h6>
-            <img class="fterImg" src="<?= $logo ?>" /> 
+        <footer class="text-center py-4">
+            <span class="page-title"><?= $title ?></span>
+            <h6 class="site-title mt-3 mb-2 ms-2">floração</h6>
+            <img class="fterImg mb-3" src="images/flor_logo.png" /><br>
+            <span>&copy; Gabriela Andrade | All rights reserved.</span> 
         </footer>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
-        <script src="<?= $js ?>"></script>
-        </body>
+        <script src="js/activelink.js"></script>
+    </body>
 </html>
