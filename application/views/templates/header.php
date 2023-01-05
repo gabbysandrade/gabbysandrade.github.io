b2b601dfa406511119d844c618339988cba9a6f9
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 12:24:01 -0600

    edit header to match floracao template

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,35 +5,31 @@ Intermediate Web Programming
 Purpose: This program will generate the header template for the
 comer o sol web application.
 --------------------------->
-        <head>
-            <title>comer o sol - <?= $title ?></title>
-            <meta name="Author" content="Gabriela Andrade">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <meta charset="utf-8">
+  <head>
+    <title><?= $title ?></title>
+    <meta name="Author" content="Gabriela Andrade">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
 
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-            <link href="<?= $css ?>" rel="stylesheet">
-            <link rel="icon" href="<?= $favicon ?>" type="image/ico">
-        </head>
-        <body>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="\"><img src="<?= $logo ?>" alt="Home"/></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarToggler">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a class="nav-link active" id="navindx" aria-current="page" href="\">Home</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" id="navabt" href="<?= $navabt ?>">About</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" id="navnews" href="<?= $navnews ?>">Recipes</a>
-                      </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link rel="icon" href="assets/images/favicon.ico" type="image/ico">
+  </head>
+  <body>
+  <nav class="navbar navbar-dark navbar-expand-lg">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="\">
+        <img src="images/flor_logo.png" alt="Home" class="d-inline-block align-text-center hdrimg"/>
+        <span class="site-title ms-3">floração</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarToggler">
+        <div class="navbar-nav me-3 mb-2 mb-lg-0">
+          <a class="nav-link active" id="navindx" aria-current="page" href="\">home</a>
+          <a class="nav-link" id="navport" href="<?= $navport ?>">portfolio</a>
+        </div>
+      </div>
+    </div>
+  </nav>
