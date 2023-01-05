14c501accb18584c3490b3fefb075b5ef376211f
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 13:43:12 -0600

    initial commit

---
+++ application/views/templates/folio_template.php
@@ -0,0 +1,7 @@
+<!--------------------------
+Name: Gabriela Andrade
+Floração Design
+Purpose: This is the HTML template view for the parser to
+use when parsing all portfolio items in the Portfolio page
+of the Floração website.
+--------------------------->
