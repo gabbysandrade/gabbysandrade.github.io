50cc4fc6f439006dbbb4dcec56c81d5d7bd686b1
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 13:21:18 -0600

    redesign portfolio controller to match floracao parameters

--- application/controllers/Portfolio.php
+++ application/controllers/Portfolio.php
@@ -6,50 +6,29 @@ Purpose: This program will create the News controller for the Recipes
 page of the comer o sol web application.
 **************************/
 
-class News extends CI_Controller {
+class Portfolio extends CI_Controller {
 
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('news_model');
+        $this->load->model('folio_model');
         $this->load->helper('url_helper');
     }
 
-    public function index()
-    {
-        $data['news'] = $this->news_model->get_news();
-        $data['title'] = 'Recipes';
-        $data['css'] = 'assets/css/main.css';
-        $data['favicon'] = 'assets/images/favicon.ico';
-        $data['logo'] = 'assets/images/logo.png';
-        $data['navabt'] = 'about';
-        $data['navnews'] = 'news';
-        $data['js'] = 'assets/js/main.js';
+    public function index() {
+        // Display all portfolio entries
+        $data['title'] = "Portfolio";
+        $data['navport'] = 'portfolio';
+
+        // Fetch portfolio from model
+        $portfolio['portfolio'] = $this->folio_model->get_folio();
+
+        // Use the parser to turn the portfolio into formatted html
+        // Include TRUE to tell parser to return and not print
+        $data['portfolio'] = $this->parser->parse('templates/folio_template', $portfolio, TRUE);
 
         $this->load->view('templates/header', $data);
-        $this->load->view('news/index', $data);
-        $this->load->view('templates/footer');
-    }
-
-    public function view($slug = NULL)
-    {
-        $data['news_item'] = $this->news_model->get_news($slug);
-
-        if (empty($data['news_item']))
-        {
-                show_404();
-        }
-    
-        $data['title'] = $data['news_item']['title'];
-        $data['css'] = '../assets/css/main.css';
-        $data['favicon'] = '../assets/images/favicon.ico';
-        $data['logo'] = '../assets/images/logo.png';
-        $data['navabt'] = '../about';
-        $data['navnews'] = '../news';
-        $data['js'] = '../assets/js/main.js';
-    
-        $this->load->view('templates/header', $data);
-        $this->load->view('news/view', $data);
+        $this->load->view('portfolio/index');
         $this->load->view('templates/footer');
     }
 }
