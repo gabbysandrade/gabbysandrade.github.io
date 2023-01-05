26325d75b55585fb2712d9c275bbc79cf1ec216d
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 13:46:49 -0600

    create portfolio card template with database variables

--- application/views/templates/folio_template.php
+++ application/views/templates/folio_template.php
@@ -5,3 +5,20 @@ Purpose: This is the HTML template view for the parser to
 use when parsing all portfolio items in the Portfolio page
 of the Floração website.
 --------------------------->
+
+<!-- Create loop for portfolio items in database -->
+{portfolio}
+<div class="col">
+    <div class="card h-100">
+        <img src="{imgsrc}" class="card-img-top" alt="{imgalt}">
+        <div class="card-body">
+            <h5 class="card-title folio-title">{title}</h5>
+            <p class="card-text text-muted">{date}</p>
+            <p class="card-text">{description}</p>
+        </div>
+        <div class="card-footer d-flex justify-content-center">
+            <a class="btn btn-primary mx-auto" href="{crdlink}" target="_blank"> view site </a>
+        </div>
+    </div>
+</div>
+{/portfolio}
