eff2b7193c75177f225999333be3419597b6af16
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 13:05:14 -0600

    rename news_model to folio_model and redesign model to match floracao parameters

---
+++ application/models/Folio_model.php
@@ -0,0 +1,32 @@
+<?php
+/**************************
+Name: Gabriela Andrade
+Intermediate Web Programming
+Purpose: This program will fetch articles from the news database for
+the comer o sol web application.
+**************************/
+
+defined('BASEPATH') OR exit('Forbidden');
+
+class Folio_model extends CI_Model {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    /*
+     * **********************************
+     * Setters
+     * **********************************
+    */
+    public function get_folio() {
+        
+        // Retrieve array of recipes from database
+        $query = $this->db->get('portfolio');
+        $portfolio = $query->result_array();
+        
+        return $portfolio;
+  }
+
+}
