31a4e979f6850078b2fceeca3cc6cbd4daed7a4a
gabbysandrade <gabbysandrade@gmail.com>
2023-01-05 13:40:43 -0600

    create portfolio view for Portfolio page

--- application/views/portfolio/index.php
+++ application/views/portfolio/index.php
@@ -1,23 +1,14 @@
 <!--------------------------
 Name: Gabriela Andrade
 Intermediate Web Programming
-Purpose: This program will generate the 'news' view for the
-Recipes page of the comer o sol web application.
+Purpose: This program will generate the 'portfolio' view for the
+Portfolio page of the Floração website.
 --------------------------->
 
-<div class="bodydiv py-5">
-    <h2 class="main-title mb-3"><?php echo $title; ?></h2>
-
-    <?php foreach ($news as $news_item): ?>
-
-        <h3 class="subtitle"><?php echo $news_item['title']; ?></h3>
-        <div class="main">
-            <h5 class="subtitle mt-3 mb-2"> Ingredients </h5>
-            <?php echo $news_item['ingredients']; ?>
-            <h5 class="subtitle my-2"> Instructions </h5>
-            <?php echo $news_item['instructions']; ?>
-        </div>
-        <p><a href="<?= '/news/'.$news_item['slug'] ?>">View Recipe</a></p>
-
-    <?php endforeach; ?>
+<div id="folio" class="container-fluid">
+    <div class="container transparent">
+        <section id="crdDeck" class="row row-cols-1 row-cols-md-3 g-4 p-5">
+            <?= $recipes ?>
+        </section>
+    </div>
 </div>
