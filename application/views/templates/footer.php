83d40259c5ece6cfbd6cc30ce838f64e38dbf2c5
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 17:47:38 -0600

    add alt to logo img

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -8,7 +8,7 @@ universal scripts for the pages.
         <footer class="text-center py-4">
             <span class="page-title"><?= $title ?></span>
             <h6 class="site-title mt-3 mb-2 ms-2">floração</h6>
-            <img class="fterImg mb-3" src="assets/images/flor_logo.png" /><br>
+            <img class="fterImg mb-3" src="assets/images/flor_logo.png" alt="Floração logo"/><br>
             <span>&copy; Gabriela Andrade | All rights reserved.</span> 
         </footer>
 
