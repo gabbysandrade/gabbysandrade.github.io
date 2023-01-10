77f90d35c30c6bfba01eba03292a5041831ec4c1
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 16:45:17 -0600

    add shade-light class to portfolio container

--- application/views/portfolio/index.php
+++ application/views/portfolio/index.php
@@ -5,7 +5,7 @@ Purpose: This program will generate the 'portfolio' view for the
 Portfolio page of the Floração website.
 --------------------------->
 
-<div id="folio" class="container-fluid">
+<div id="folio" class="container-fluid shade-light">
     <div class="container transparent">
         <section id="crdDeck" class="row row-cols-1 row-cols-md-3 g-4 p-5">
             <?= $portfolio ?>
