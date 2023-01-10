9b58190077258d02c2056fa2cf82ebb97f0be148
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 17:32:30 -0600

    change 'Hello' to 'Bem-vindo'

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -12,7 +12,7 @@ home page of the Floração website.
                 <img id="introimg" src="assets/images/intro.png" alt="Gabriela Andrade, Web Developer & Designer" />
             </div>
             <div class="col-12 col-md-5 mt-5">
-                <h5 class="txt-light2">Hello, I'm Gabriela,</h5>
+                <h5 class="txt-light2">Bem-vindo, I'm Gabriela,</h5>
                 <h1 class="txt-vivid txt-shadow">Professional Web Developer & UI/UX Designer</h1>
                 <hr class="hr-hlf">
                 <a class="socials" href="https://www.linkedin.com/in/gabriela-s-andrade/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
