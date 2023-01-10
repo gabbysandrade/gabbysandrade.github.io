3673ad30ee888f03f05a268c94411ff2dd8e8757
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 13:24:13 -0600

    add containing div for background

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -5,16 +5,18 @@ Purpose: This program will generate the main body content for the
 home page of the Floração website.
 --------------------------->
 
-<div class="container bodydiv">
-    <div class="row mb-3">
-        <div class="col-12 col-md-4">
-            <img id="introimg" src="assets/images/intro.png" alt="Gabriela Andrade, Web Developer & Designer" />
-        </div>
-        <div class="col-12 col-md-8">
-            <h5>Hello, I'm Gabriela,</h5>
-            <h1>Professional Web Developer & UI/UX Designer</h1>
-            <hr>
-            <a class="socials" href="https://www.linkedin.com/in/gabriela-s-andrade/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+<div class="container shade-light">
+    <div class="container bodydiv">
+        <div class="row mb-3">
+            <div class="col-12 col-md-4">
+                <img id="introimg" src="assets/images/intro.png" alt="Gabriela Andrade, Web Developer & Designer" />
+            </div>
+            <div class="col-12 col-md-8">
+                <h5>Hello, I'm Gabriela,</h5>
+                <h1>Professional Web Developer & UI/UX Designer</h1>
+                <hr>
+                <a class="socials" href="https://www.linkedin.com/in/gabriela-s-andrade/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
         </div>
     </div>
 </div>
