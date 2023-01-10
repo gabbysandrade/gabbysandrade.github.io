08f9af9614d1a3af26a03c80e404f7ecbe1c6c35
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 13:11:33 -0600

    add fontawesome script to header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -14,6 +14,7 @@ navbar for the pages.
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="assets/css/main.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/3b1edbd6c5.js" crossorigin="anonymous"></script>
     <link rel="icon" href="assets/images/favicon.ico" type="image/ico">
   </head>
   <body>
