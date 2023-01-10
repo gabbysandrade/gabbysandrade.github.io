f19dce08d50fd6fcbbcd280b0a06b5da61d6a08a
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 13:08:52 -0600

    set width for intro img

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -8,7 +8,7 @@ home page of the Floração website.
 <div class="container bodydiv">
     <div class="row mb-3">
         <div class="col-12 col-md-4">
-            <img src="assets/images/intro.png" alt="Gabriela Andrade, Web Developer & Designer" />
+            <img id="introimg" src="assets/images/intro.png" alt="Gabriela Andrade, Web Developer & Designer" />
         </div>
         <div class="col-12 col-md-8">
             <h5>Hello, I'm Gabriela,</h5>
