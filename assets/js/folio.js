e99117ea09a24c1213724d124e7d4704d51bd37c
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 16:57:13 -0600

    initial commit

---
+++ assets/js/folio.js
@@ -0,0 +1,9 @@
+"use strict";
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This script will serve the Portfolio page of the Floração
+website. It will move the class of "active" to the appropriate
+link in the navbar depending upon the page title and will animate
+the portfolio cards upon page load.
+**************************/
