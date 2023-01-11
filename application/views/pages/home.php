8d8c88c7781630481774d23decbec731644914de
gabbysandrade <gabbysandrade@gmail.com>
2023-01-10 18:04:15 -0600

    add button link to portfolio

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -14,6 +14,7 @@ home page of the Floração website.
             <div class="col-12 col-md-5 mt-5">
                 <h5 class="txt-light2">Bem-vindo, I'm Gabriela,</h5>
                 <h1 class="txt-vivid txt-shadow">Professional Web Developer & UI/UX Designer</h1>
+                <a class="btn btn-outline-light btn-lg mt-4" href="<?= $navport ?>">view my portfolio &#8594;</a>
                 <hr class="hr-hlf">
                 <a class="socials" href="https://www.linkedin.com/in/gabriela-s-andrade/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
             </div>
