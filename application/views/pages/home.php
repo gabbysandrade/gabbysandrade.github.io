363fee5a050d1b1cd7a8fd4115fb09e7c9e2dc3a
gabbysandrade <gabbysandrade@gmail.com>
2023-01-11 12:17:06 -0600

    adjust formdiv padding

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -41,7 +41,7 @@ home page of the Floração website.
 </div>
 
 <div class="shade-light py-5">
-    <div id="formdiv" class="mx-auto bodydiv p-5">
+    <div id="formdiv" class="mx-auto bodydiv pt-3 pb-5 px-5">
         <div id="msg" class="text-center mx-auto">
             <br>
         </div>
