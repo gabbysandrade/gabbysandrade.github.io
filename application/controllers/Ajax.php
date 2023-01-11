f68317ad2282538f384c86a6fe81078f81b8dd33
gabbysandrade <gabbysandrade@gmail.com>
2023-01-11 14:37:58 -0600

    initial commit

---
+++ application/controllers/Ajax.php
@@ -0,0 +1,7 @@
+<?php
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This program will create the Ajax controller for the Contact
+form of the Floração website.
+**************************/
