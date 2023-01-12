f7f90928c1df11bab202bfcb9be4c325a944c7a7
gabbysandrade <gabbysandrade@gmail.com>
2023-01-12 12:12:40 -0600

    remove debug statements

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -7,14 +7,6 @@ form of the Floração website.
 **************************/
 class Ajax extends CI_Controller {
 
-    public function debug_to_console($data) {
-        $output = $data;
-        if (is_array($output))
-            $output = implode(',', $output);
-    
-        echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
-    }
-
     public function processnames() {
 
         // Ensure caller is an AJAX request
@@ -26,23 +18,11 @@ class Ajax extends CI_Controller {
             $message = strip_tags(trim($this->input->post('frmmsg')));
             $from = strip_tags(trim($this->input->post('email')));
 
-            debug_to_console("Name:");
-            debug_to_console($name);
-            debug_to_console("Subject:");
-            debug_to_console($subject);
-            debug_to_console("Message:");
-            debug_to_console($message);
-            debug_to_console("From:");
-            debug_to_console($from);
-
             // Use PHP filter for email input
             $from = filter_var($from, FILTER_VALIDATE_EMAIL, FILTER_NULL_ON_FAILURE);
-            debug_to_console("From after filter:");
-            debug_to_console($from);
     
             // Process data
             if(!empty($name) && !empty($subject) && !empty($message) && !empty($from)){
-                debug_to_console("Data processing has begun");
 
                 // Form the email headers
                 $headers = "From: $from\r\n";
