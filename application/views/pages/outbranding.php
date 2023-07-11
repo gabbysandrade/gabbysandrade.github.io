d15db65c0983045bd60608a01f636011e88239f4
gabbysandrade <gabbysandrade@gmail.com>
2023-07-11 11:47:40 -0500

    add shade-light class to intro section and move all other classes to an inner div

--- application/views/pages/outbranding.php
+++ application/views/pages/outbranding.php
@@ -8,40 +8,42 @@ OutBranding Promos portfolio detail page of the Floração website.
 <div class="detail-banner">
     <img class="banner" src="assets/images/banner_obp.png" alt=""/>
 </div>
-<section class="container bodydiv py-5 txt-light1">
-    <h1>OutBranding Promos</h1>
-    <div class="row">
-        <div class="col-12 col-md-6">
-            <p>Now armed with a year's worth of experience with the brand and the team behind it, this second 
-            redesign of the OutBranding Promos website was a complete departure stylistically from the redesign completed when I 
-            was first brought onto the team. OutBranding Promos specializes in a concierge, individualized experience for every 
-            customer, and so the design focus centered around the humans behind OutBranding. From both user experience and sales 
-            perspectives, the key was to engender as many opportunities for connection  as possible between users and the team 
-            members behind the brand.</p>
-        </div>
-        <div class="col-12 col-md-6 stats">
-            <div class="row">
-                <div class="col-4">
-                    <img class="stat-icon" src="assets/images/calendar_icon.png" alt="Publish Date"/>
-                </div>
-                <div class="col-8 ps-3">
-                    <p>June 2023</p>
-                </div>
+<section class="shade-light">
+    <div class="container bodydiv py-5 txt-light1">
+        <h1>OutBranding Promos</h1>
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <p>Now armed with a year's worth of experience with the brand and the team behind it, this second 
+                redesign of the OutBranding Promos website was a complete departure stylistically from the redesign completed when I 
+                was first brought onto the team. OutBranding Promos specializes in a concierge, individualized experience for every 
+                customer, and so the design focus centered around the humans behind OutBranding. From both user experience and sales 
+                perspectives, the key was to engender as many opportunities for connection  as possible between users and the team 
+                members behind the brand.</p>
             </div>
-            <div class="row">
-                <div class="col-4">
-                    <img class="stat-icon" src="assets/images/laptop_icon.png" alt="Languages, Frameworks, and Tools Used"/>
+            <div class="col-12 col-md-6 stats">
+                <div class="row">
+                    <div class="col-4">
+                        <img class="stat-icon" src="assets/images/calendar_icon.png" alt="Publish Date"/>
+                    </div>
+                    <div class="col-8 ps-3">
+                        <p>June 2023</p>
+                    </div>
                 </div>
-                <div class="col-8 ps-3">
-                    <p>June 2023</p>
+                <div class="row">
+                    <div class="col-4">
+                        <img class="stat-icon" src="assets/images/laptop_icon.png" alt="Languages, Frameworks, and Tools Used"/>
+                    </div>
+                    <div class="col-8 ps-3">
+                        <p>June 2023</p>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-4">
-                    <img class="stat-icon" src="assets/images/palette_icon.png" alt="Color Palette"/>
-                </div>
-                <div class="col-8 ps-3">
-                    <img class="palettebar" src="assets/images/palettebar_obp.png" alt="OutBranding Promos Color Palette"/>
+                <div class="row">
+                    <div class="col-4">
+                        <img class="stat-icon" src="assets/images/palette_icon.png" alt="Color Palette"/>
+                    </div>
+                    <div class="col-8 ps-3">
+                        <img class="palettebar" src="assets/images/palettebar_obp.png" alt="OutBranding Promos Color Palette"/>
+                    </div>
                 </div>
             </div>
         </div>
