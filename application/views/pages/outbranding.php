54e7af82b764707e2f8075d4693084abca0ff745
gabbysandrade <gabbysandrade@gmail.com>
2023-07-11 09:52:38 -0500

    initial commit

---
+++ application/views/pages/outbranding.php
@@ -0,0 +1,6 @@
+<!--------------------------
+Name: Gabriela Andrade
+Floração Design
+Purpose: This program will generate the main body content for the
+OutBranding Promos portfolio detail page of the Floração website.
+--------------------------->
