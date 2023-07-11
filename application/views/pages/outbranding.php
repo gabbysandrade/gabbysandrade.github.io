1053709d8bd04351bd787d45d68caad412b892b0
gabbysandrade <gabbysandrade@gmail.com>
2023-07-11 12:00:53 -0500

    add details section

--- application/views/pages/outbranding.php
+++ application/views/pages/outbranding.php
@@ -11,7 +11,7 @@ OutBranding Promos portfolio detail page of the Floração website.
 <section class="shade-light">
     <div class="container bodydiv py-5 txt-light1">
         <h1>OutBranding Promos</h1>
-        <div class="row">
+        <div class="row align-items-center">
             <div class="col-12 col-md-6">
                 <p>Now armed with a year's worth of experience with the brand and the team behind it, this second 
                 redesign of the OutBranding Promos website was a complete departure stylistically from the redesign completed when I 
@@ -21,7 +21,7 @@ OutBranding Promos portfolio detail page of the Floração website.
                 members behind the brand.</p>
             </div>
             <div class="col-12 col-md-6 stats">
-                <div class="row">
+                <div class="row align-items-center">
                     <div class="col-4">
                         <img class="stat-icon" src="assets/images/calendar_icon.png" alt="Publish Date"/>
                     </div>
@@ -29,7 +29,7 @@ OutBranding Promos portfolio detail page of the Floração website.
                         <p>June 2023</p>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row align-items-center">
                     <div class="col-4">
                         <img class="stat-icon" src="assets/images/laptop_icon.png" alt="Languages, Frameworks, and Tools Used"/>
                     </div>
@@ -37,7 +37,7 @@ OutBranding Promos portfolio detail page of the Floração website.
                         <p>June 2023</p>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row align-items-center">
                     <div class="col-4">
                         <img class="stat-icon" src="assets/images/palette_icon.png" alt="Color Palette"/>
                     </div>
@@ -45,6 +45,23 @@ OutBranding Promos portfolio detail page of the Floração website.
                         <img class="palettebar" src="assets/images/palettebar_obp.png" alt="OutBranding Promos Color Palette"/>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="highlight-bl">
+    <div class="container bodydiv py-3 txt-dark">
+        <div class="row align-items-center">
+            <div class="col-12 col-md-6">
+                <img class="col-img" src="assets/images/detail_obp.png" alt="Comparison of the initial OutBranding Promos website redesign to the latest redesign." />
+            </div>
+            <div class="col-12 col-md-6 text-center">
+                <h2>Hasta la vista, vectors!</h2>
+                <p>The previous OutBranding redesign focused on a vector-graphic style to create a cohesive design language throughout the site and speak to the fun 
+                and creative aspects of the brand.</p>
+                <p>In this redesign, we reframed the vector graphics as highlights rather than the central style in favor of product and team member photos to achieve 
+                a more sleek and professional look that better answers the question, “What can customers expect when they select OutBranding Promos as their 
+                promotional product service?”</p>
             </div>
         </div>
     </div>
