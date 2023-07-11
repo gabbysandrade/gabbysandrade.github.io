971accd70482b2ecd67e3d744446b435983f4146
gabbysandrade <gabbysandrade@gmail.com>
2023-07-11 11:29:33 -0500

    add intro section:

--- application/views/pages/outbranding.php
+++ application/views/pages/outbranding.php
@@ -4,3 +4,46 @@ Floração Design
 Purpose: This program will generate the main body content for the
 OutBranding Promos portfolio detail page of the Floração website.
 --------------------------->
+
+<div class="detail-banner">
+    <img class="banner" src="assets/images/banner_obp.png" alt=""/>
+</div>
+<div class="container bodydiv py-5 txt-light1">
+    <h1>OutBranding Promos</h1>
+    <div class="row">
+        <div class="col-12 col-md-6">
+            <p>Now armed with a year's worth of experience with the brand and the team behind it, this second 
+            redesign of the OutBranding Promos website was a complete departure stylistically from the redesign completed when I 
+            was first brought onto the team. OutBranding Promos specializes in a concierge, individualized experience for every 
+            customer, and so the design focus centered around the humans behind OutBranding. From both user experience and sales 
+            perspectives, the key was to engender as many opportunities for connection  as possible between users and the team 
+            members behind the brand.</p>
+        </div>
+        <div class="col-12 col-md-6 stats">
+            <div class="row">
+                <div class="col-4">
+                    <img class="stat-icon" src="assets/images/calendar_icon.png" alt="Publish Date"/>
+                </div>
+                <div class="col-8 ps-3">
+                    <p>June 2023</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img class="stat-icon" src="assets/images/laptop_icon.png" alt="Languages, Frameworks, and Tools Used"/>
+                </div>
+                <div class="col-8 ps-3">
+                    <p>June 2023</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <img class="stat-icon" src="assets/images/palette_icon.png" alt="Color Palette"/>
+                </div>
+                <div class="col-8 ps-3">
+                    <img class="palettebar" src="assets/images/palettebar_obp.png" alt="OutBranding Promos Color Palette"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
