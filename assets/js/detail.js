17bcce5bef7e61ac4af46c02c0728df236dc5561
gabbysandrade <gabbysandrade@gmail.com>
2023-07-11 20:23:13 -0500

    initial commit

---
+++ assets/js/detail.js
@@ -0,0 +1,9 @@
+"use strict";
+/**************************
+Name: Gabriela Andrade
+Floração Design
+Purpose: This script will serve the Portfolio Detail pages of the 
+Floração website. It will move the class of "active" to the appropriate
+link in the navbar depending upon the page title and will initiate
+the popover animations.
+**************************/
