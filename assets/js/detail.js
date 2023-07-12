240fff3ed1766b5c153ebfd8ec1a6d6e8b5ca04e
gabbysandrade <gabbysandrade@gmail.com>
2023-07-11 20:23:49 -0500

    add page-title function

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -7,3 +7,27 @@ Floração website. It will move the class of "active" to the appropriate
 link in the navbar depending upon the page title and will initiate
 the popover animations.
 **************************/
+
+$(document).ready(function(){
+    // Check page title
+    console.log($(".page-title").text());
+    switch ($(".page-title").text()) {
+        case "Home":
+            // Leave homepage as active link
+            break;
+        case "Portfolio":
+            // Remove active class from home nav link
+            $("#navindx").removeClass("active");
+            // Add active class to Portfolio nav link
+            $("#navport").addClass("active");
+            break;
+        case "ERROR":
+            // Remove active class from home nav link
+            $("#navindx").removeClass("active");
+            break;
+        default:
+            // Remove active class from home nav link
+            $("#navindx").removeClass("active");
+            break;
+    }   
+});
