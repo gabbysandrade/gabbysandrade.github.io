5e07b56d1999d295938403fcb290df4d2582c097
gabbysandrade <gabbysandrade@gmail.com>
2023-07-14 21:54:41 -0500

    initial commit

---
+++ application/views/pages/hhtax.php
@@ -0,0 +1,6 @@
+<!--------------------------
+Name: Gabriela Andrade
+Floração Design
+Purpose: This program will generate the main body content for the
+H&H Tax Services Texas portfolio detail page of the Floração website.
+--------------------------->
