b93c529a6f89f1901c3fdca63742a98a00f2283b
gabbysandrade <gabbysandrade@gmail.com>
2023-07-17 11:58:43 -0500

    add banner

--- application/views/pages/hhtax.php
+++ application/views/pages/hhtax.php
@@ -4,3 +4,6 @@ Floração Design
 Purpose: This program will generate the main body content for the
 H&H Tax Services Texas portfolio detail page of the Floração website.
 --------------------------->
+<div class="detail-banner">
+    <img class="banner" src="assets/images/banner_hh.svg" alt=""/>
+</div>
