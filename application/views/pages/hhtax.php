480a9103ad5408b60d3727fc497f3da5fb38d949
gabbysandrade <gabbysandrade@gmail.com>
2023-07-17 12:10:54 -0500

    add overview section:

--- application/views/pages/hhtax.php
+++ application/views/pages/hhtax.php
@@ -7,3 +7,46 @@ H&H Tax Services Texas portfolio detail page of the Floração website.
 <div class="detail-banner">
     <img class="banner" src="assets/images/banner_hh.svg" alt=""/>
 </div>
+<section class="shade-light">
+    <div class="container bodydiv py-5 txt-light1">
+        <h1 class="overview-title">H&H Tax Service Texas</h1>
+        <div class="row my-4 align-items-center">
+            <div class="col-12 col-md-6 mb-3">
+                <p class="overview">The only design parameters received for this proudly Texan-born business were the color scheme of 
+                “red, white, and blue,” and the request for something simple and secure to IRS standards. Based on these terms, the 
+                company values, and marketing materials from previous years, I worked closely with the owner to develop a brand image 
+                for the business which informed the design of the site as well as their custom logo. The other aim of the site was to 
+                ease the burden on my client as their business grew - to present information and secure means of uploading documents 
+                in accessible and easily digestible formats for their customers in order to streamline their processes and ensure 
+                clients were adequately prepared for appointments.</p>
+            </div>
+            <div class="col-12 col-md-4 offset-md-2 mb-3 p-3 stats">
+                <div class="row align-items-center">
+                    <div class="col-3">
+                        <img class="stat-icon" src="assets/images/calendar_icon.png" alt="Publish Date"/>
+                    </div>
+                    <div class="col-9 ps-4">
+                        <p>September 2022</p>
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-3">
+                        <img class="stat-icon" src="assets/images/laptop_icon.png" alt="Languages, Frameworks, and Tools Used"/>
+                    </div>
+                    <div class="col-9 ps-4">
+                        <p>Wordpress, HTML/CSS, jQuery, Canva</p>
+                    </div>
+                </div>
+                <div class="row align-items-center">
+                    <div class="col-3">
+                        <img class="stat-icon" src="assets/images/palette_icon.png" alt="Color Palette"/>
+                    </div>
+                    <div class="col-9 ps-4">
+                        <img class="palettebar" src="assets/images/palettebar_hh.png" alt="H&H Tax Service Texas Color Palette"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="btn btn-outline-light btn-lg" href="https://hhtaxservice1040.com/" target="_blank">view site</a>
+    </div>
+</section>
